06690d8af5e949470ba268282de7fff254ab1f4f
Damian Osiecki <mlodybarney@gmail.com>
2018-09-27 22:46:08 +0200

    Fix animacji. Fix wyboru języka i wyszukiwarki słów. Dodanie animacji po przycisnięciu toolbara. Sidebar zmniejsza się do samych ikon. Main content sie rozszeża.

--- gulp/app/js/main.js
+++ gulp/app/js/main.js
@@ -2,3 +2,6 @@ $('.collapse').collapse();
 $(".navbar-nav-search-icon").click(function(){
     $(".input-search").toggle("slow");
  });
+ $( ".navbar-toggler").click(function() {
+    $("body").toggleClass("sidebar-icon-only");
+   });
