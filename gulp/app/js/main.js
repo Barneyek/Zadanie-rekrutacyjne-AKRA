b445493301de954037f2382d9209d68b789b5446
Damian Osiecki <mlodybarney@gmail.com>
2018-09-28 22:38:48 +0200

    test

--- gulp/app/js/main.js
+++ gulp/app/js/main.js
@@ -1,7 +1,19 @@
-$('.collapse').collapse();
+$('.navbar-toggler').collapse();
 $(".navbar-nav-search-icon").click(function(){
     $(".input-search").toggle("slow");
  });
  $( ".navbar-toggler").click(function() {
     $("body").toggleClass("sidebar-icon-only");
    });
+$( ".nav-item").click(function() {
+    if ($("body").hasClass("sidebar-icon-only")) {
+       
+            $( this ).toggleClass("hover-open");
+   
+        }
+    else{
+        console.log("nie");         
+        }
+   
+    });
+  
